176273eef289100ef44394643acd930e98b82bd6
tobias <tobias.guerrero_c@mail.udp.cl>
2023-04-09 00:46:45 -0400

    ultimo avance

--- src/grafico.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Gráfico de tiempos de las llamadas a la API</title>
-<script src="https
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const axios = require("axios");
-const redis = require("redis");
-const responseTime = require("response-time");
-const app = express();
+const express = require('express');
+const axios = require('axios');
+const redis = require('redis');
+const responseTime = require('response-time');
+const {promisify} = require('util')
 
 //Connecting to redis
 const client = redis.createClient({
@@ -10,33 +10,59 @@ const client = redis.createClient({
     port: 6379,
 });
 
+const GET_ASYNC = promisify(client.get).bind(client)
+const SET_ASYNC = promisify(client.set).bind(client)
+
+const app = express();
+
 (async () => {
     await client.connect(); 
 });
 
-
-client.on("ready", () => {
-    console.log("Connected succesfully");
+client.on('ready', () => {
+    console.log('Connected succesfully');
 });
 
 client.on("error", (err) => {
-    console.log(err);
+    console.log(`${err}`);
 });
 
 app.use(responseTime());
 
 app.get("/games", async (req, res) => {
-    const response = await axios.get("https://www.freetogame.com/api/games");
+    try{
+         //Response from cache
+        const reply = await GET_ASYNC("games");
+        if (reply) return res.json(JSON.parse(reply));
 
-    client.set('games', JSON.stringify(response.data), (err, reply) => {
-        if (err) console.log(reply);
+        const response = await axios.get(
+        "https://www.freetogame.com/api/games"
+        );
 
-        console.log(reply);
-
+        await SET_ASYNC("games", JSON.stringify(response.data));
         res.json(response.data);
+    } catch (error) {
+        console.log(error);
+    }
     });
-});
 
+    app.get("/games?:category", async (req, res) => {
+        try{
+            const reply = await GET_ASYNC(req.params.category);
+            //const reply = await GET_ASYNC(req.originalUrl);
+            if (reply) return res.json(JSON.parse(reply));    
+
+            const response = await axios.get(
+                'https://www.freetogame.com/api/games?' + req.params.category
+                );
+
+            await SET_ASYNC(req.params.category, JSON.stringify(response.data)); 
+                
+            return res.json(response.data);    
+        } catch (error) {
+            return res.status(error.response.status).json({ message: error.message });
+        }
+        });    
 
 app.listen(3000);
 console.log("Server listen on port 3000");
