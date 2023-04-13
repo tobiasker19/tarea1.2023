8516a538506cddb02754d85b955b2c6c7673665a
unknown <icaceressatorres2001@gmail.com>
2023-04-12 21:49:16 -0400

    actualizacion

--- src/api_call.js
+++ src/api_call.js
@@ -4,25 +4,39 @@ const axios = require('axios');
 const BASE_URL = 'https://www.freetogame.com/api';
 //const BASE_URL = 'http://localhost:3000';
 // Definimos el número de solicitudes a realizar
-const NUM_REQUESTS = 5000;
+const NUM_REQUESTS = 50;
 
 // Creamos una función para realizar una solicitud a la API con un ID aleatorio
 async function makeRequest() {
-  const id = Math.floor(Math.random() * 550) + 1; // Generamos un ID aleatorio entre 1 y 550
-  const start = Date.now(); // Registramos el tiempo de inicio de la solicitud
-  //const response = await axios.get(`${BASE_URL}/game/${id}`); // Realizamos la solicitud HTTP con el ID aleatorio
-  const response = await axios.get(`${BASE_URL}/game?id=${id}`); // Realizamos la solicitud HTTP con el ID aleatorio
-  const end = Date.now(); // Registramos el tiempo de finalización de la solicitud
-  return { data: response.data, time: end - start }; // Devolvemos los datos y el tiempo de respuesta
+  const id = Math.floor(Math.random() * 550) + 1;
+  const start = Date.now();
+  
+  try {
+    const response = await axios.get(`${BASE_URL}/game?id=${id}`);
+    const end = Date.now();
+    return { data: response.data, time: end - start };
+  } catch (error) {
+    // Si la solicitud falla, devolver un objeto vacío
+    //console.error(`Error al solicitar el juego con ID ${id}: ${error}`);
+    return { data: {}, time: 0 };
+  }
 }
+
 
 // Realizamos las solicitudes en bucle
 async function run() {
   const results = []; // Creamos una matriz vacía para almacenar los resultados
-  for (let i = 0; i < NUM_REQUESTS; i++) {
+  for (let i = 1; i <= NUM_REQUESTS; i++) {
     const { data, time } = await makeRequest();
-    console.log(`Solicitud ${i}: ID ${data.id}, ${time}ms`);
-    results.push(time); // Agregamos el tiempo de respuesta a la matriz de resultados
+    
+    if(time != 0){
+      console.log(`Solicitud ${i}: ID ${data.id}, ${time}ms`);
+      results.push(time); // Agregamos el tiempo de respuesta a la matriz de resultados
+    }
+    else{
+      i=i-1;
+    }
+    
   }
   console.log(results); // Imprimimos la matriz de resultados en la consola
 }
